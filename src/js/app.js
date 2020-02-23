8cf2ae63940ab0f4a3ac0ad445fd5974ba776d86
alchimic <alchimicus@gmail.com>
2020-02-23 18:06:43 +0200

    start

--- src/js/app.js
+++ src/js/app.js
@@ -1,145 +1,217 @@
 import * as THREE from 'three';
-import { TimelineMax } from 'gsap';
-var OrbitControls = require('three-orbit-controls')(THREE);
+
 import fragment from './fragment.glsl';
 import vertex from './vertex.glsl';
-
-// import './lib/curves.js';
-
-
-// console.log(THREE.Curves.TorusKnot);
+import * as dat from 'dat.gui';
 
 
-
-let camera, pos, controls, scene, renderer, geometry, geometry1, material, plane, another;
-let destination = { x: 0, y: 0 };
-let textures = [];
-
-function init() {
-  scene = new THREE.Scene();
-  scene.destination = { x: 0, y: 0 };
-
-  renderer = new THREE.WebGLRenderer({ alpha: true });
-
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerWidth);
-
-  var container = document.getElementById('container');
-  container.appendChild(renderer.domElement);
-
-  camera = new THREE.PerspectiveCamera(
-    70,
-    window.innerWidth / window.innerHeight,
-    0.001, 100
-  );
-  camera.position.set(0, 0, 32);
+import { TimelineMax } from 'gsap';
+let OrbitControls = require('three-orbit-controls')(THREE);
 
 
-  controls = new OrbitControls(camera, renderer.domElement);
+export default class Sketch {
+  constructor(selector) {
+    this.scene = new THREE.Scene();
+
+    this.renderer = new THREE.Scene();
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setClearColor(0xeeeeee, 1);
+
+    this.container = document.getElementById('container');
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.container.appendChild(this.renderer.domElement);
+
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      window.innerWidth / window.innerHeight,
+      0.001,
+      1000
+    );
 
 
-  material = new THREE.ShaderMaterial({
-    side: THREE.DoubleSide,
-    uniforms: {
-      time: { type: 'f', value: 0 }
-    },
-    // wireframe: true,
-    vertexShader: vertex,
-    fragmentShader: fragment,
-  });
+    this.camera.position.set(0, 0, 2);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.time = 0;
 
-  //new THREE.PlaneGeometry( 1,1, 64, 64 )
+    this.paused = false;
 
-  function CustomSinCurve(scale) {
 
-    THREE.Curve.call(this);
 
-    this.scale = (scale === undefined) ? 1 : scale;
-
+    this.setupResize();
+    this.addObjects();
+    this.resize();
+    // this.settings();
   }
 
-  CustomSinCurve.prototype = Object.create(THREE.Curve.prototype);
-  CustomSinCurve.prototype.constructor = CustomSinCurve;
+  settings() {
+    let that = this;
+    this.settings = {
+      time: 0,
+    };
 
-  CustomSinCurve.prototype.getPoint = function(t) {
+    this.gui = new dat.GUI();
+    this.gui.add(this.settings, 'time', 0, 100, 0.01);
+  }
 
-    t = (Math.PI * 2) * t;
-    var s = Math.sin(t);
-    var c = Math.cos(t);
-    var r = 2 + 6 * c;
-    var ty = 1 + (-r * c) * 0.205 - 0.25;
-    var tx = (-r * s) * 0.205;
-    var tz = s * 0.65;
+  setupResize() {
+    window.addEventListener('resize', this.resize.bind(this));
+  }
 
-    return new THREE.Vector3(tx, ty, tz).multiplyScalar(this.scale);
-
-  };
-
-
-  // first object
-  let path = new CustomSinCurve(10);
-  geometry = new THREE.TubeGeometry(path, 100, 3, 100, true);
-  plane = new THREE.Mesh(geometry, material);
-  plane.position.x = 10;
-  plane.rotation.y = -Math.PI * 0.5;
-  scene.add(plane);
+  resize() {
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height;
 
 
+    //image cover
+    // this.imageAspect = 853 / 1280;
+    // let a1;
+    // let a2;
+    // if| (this.height / this.width > this.imageAspect) {
+    //   a1 = (this.width / this.height) * this.imageAspect;
+    //   a2 = 1;
+    // } else {
+    //   a1 = 1;
+    //   a2 = (this.width / this.height) * this.imageAspect;
+    // }
 
-  // second object
-  // path = new THREE.Curves.KnotCurve();
-  // let geometry1 = new THREE.TubeGeometry(path, 100, 1.5, 100, true);
-  // another = new THREE.Mesh(geometry1, material);
-  // another.position.x = -16;
-  // another.rotation.x = -Math.PI * 0.5;
-  // another.rotation.z = -Math.PI * 0.45;
-  // another.rotation.y = Math.PI * 0.1;
-  // scene.add(another);
 
-  // resize();
+    // this.material.uniforms.resolution.value.x = this.width;
+    // this.material.uniforms.resolution.value.y = this.height;
+    // this.material.uniforms.resolution.value.z = a1;
+    // this.material.uniforms.resolution.value.w = a2;
 
 
+    this.camera.updateProjectionMatrix();
+  }
+
+  addObjects() {
+    let that = this;
+    this.material = new THREE.ShaderMaterial({
+      extensions: {
+        derivatives: '#extension GL_OES_standard_derivatives: enable'
+      },
+      side: THREE.DoubleSide,
+      uniforms: {
+        time: {
+          type: 'f', value: 0
+        },
+        resolution: {
+          type: 'v4', value: new THREE.Vector4()
+        },
+        uvRate1: {
+          value: new THREE.Vector2(1, 1)
+        }
+      },
+      // wireframe: true,
+      // transparent: true,
+
+      vertexShader: vertex,
+      fragmentShader: fragment
+    });
+
+    this.geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
+    this.geometry = new THREE.OctahedronBufferGeometry(1);
+    // this.geometry = new THREE.OctahedronBoxBufferGeometry(1);
+
+    this.plane = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.plane);
+  }
+
+  stop() {
+    this.paused = true;
+  }
+
+  play() {
+    this.paused = false;
+    this.render();
+  }
+
+  render() {
+    renderer.render(scene, camera);
+  }
 }
 
-window.addEventListener('resize', resize);
-function resize() {
-  var w = window.innerWidth;
-  var h = window.innerHeight;
-  renderer.setSize(w, h);
-  camera.aspect = w / h;
-  camera.updateProjectionMatrix();
-}
-
-let time = 0;
-function animate() {
-  time = time + 0.05;
-  material.uniforms.time.value = time;
-
-  // plane.rotation.y = time/50;
-
-  requestAnimationFrame(animate);
-  render();
-}
-
-function render() {
-  scene.rotation.x += (scene.destination.x - scene.rotation.x) * 0.05;
-  scene.rotation.y += (scene.destination.y - scene.rotation.y) * 0.05;
-  renderer.render(scene, camera);
-}
-
-let ww = window.innerWidth;
-let wh = window.innerHeight;
-function onMousemove(e) {
-  var x = (e.clientX - ww / 2) / (ww / 2);
-  var y = (e.clientY - wh / 2) / (wh / 2);
-  scene.destination.x = y * 0.5;
-  scene.destination.y = x * 0.5;
-}
-window.addEventListener('mousemove', onMousemove);
-
-init();
-animate();
+new Sketch('container');
 
 
 
+
+// let camera, pos, controls, scene, renderer, geometry, geometry1, material, plane, tex1, tex2;
+// let destination = { x: 0, y: 0 };
+// let textures = [];
+
+// function init() {
+//   scene = new THREE.Scene();
+
+//   renderer = new THREE.WebGLRenderer();
+
+//   renderer.setPixelRatio(window.devicePixelRatio);
+//   renderer.setSize(window.innerWidth, window.innerWidth);
+
+//   var container = document.getElementById('container');
+//   container.appendChild(renderer.domElement);
+
+//   camera = new THREE.PerspectiveCamera(
+//     70,
+//     window.innerWidth / window.innerHeight,
+//     0.001, 100
+//   );
+//   camera.position.set(0, 0, 1);
+
+
+//   controls = new OrbitControls(camera, renderer.domElement);
+
+
+//   material = new THREE.ShaderMaterial({
+//     side: THREE.DoubleSide,
+//     uniforms: {
+//       time: { type: 'f', value: 0 }
+//     },
+//     // wireframe: true,
+//     vertexShader: vertex,
+//     fragmentShader: fragment,
+//   });
+
+//   plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1, 64, 64), material);
+//   scene.add(plane);
+
+//   resize();
+
+
+// }
+
+// window.addEventListener('resize', resize);
+// function resize() {
+//   var w = window.innerWidth;
+//   var h = window.innerHeight;
+//   renderer.setSize(w, h);
+//   camera.aspect = w / h;
+//   camera.updateProjectionMatrix();
+// }
+
+// let time = 0;
+// function animate() {
+//   time = time + 0.05;
+//   material.uniforms.time.value = time;
+
+//   requestAnimationFrame(animate);
+//   render();
+// }
+
+// function render() {
+//   renderer.render(scene, camera);
+// }
+
+
+// init();
+// animate();
 
